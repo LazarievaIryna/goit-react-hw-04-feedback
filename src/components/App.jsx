a4b07d42c615384097f3bbdc06e24fd95f953487
Iryna Lazareva <il337733@gmail.com>
2023-05-17 10:54:18 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
   };
 
   const total = countTotalFeedback();
-  // console.log(total);
+  
   const percent = countPositiveFeedbackPercentage();
   return (
     <>
@@ -63,65 +63,3 @@ export default function App() {
   );
 }
 
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   //функция на кнопку
-//   handleFeedback = e => {
-//     const vote = e.target.name;
-//     console.log(vote);
-//     this.setState(prevState => ({ [vote]: prevState[vote] + 1 }));
-//   };
-
-//   //функция на общую сумму
-// countTotalFeedback = () => {
-//   const { good, neutral, bad } = this.state;
-//   // console.log(Object.values(this.state));
-//   const totalFeedback = good + neutral + bad;
-//   return totalFeedback;
-// };
-
-//   // %
-// countPositiveFeedbackPercentage = () => {
-//   const { good } = this.state;
-//   const positivePercent = Math.round(
-//     (good / this.countTotalFeedback()) * 100
-//   );
-//   return positivePercent;
-// };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const options = Object.keys(this.state);
-//     console.log(options);
-//     const total = this.countTotalFeedback();
-//     const percent = this.countPositiveFeedbackPercentage();
-//     return (
-//       <>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={options}
-//             onLeaveFeedback={this.handleFeedback}
-//           />
-//         </Section>
-
-//         <Section title="Statistics">
-//           {total === 0 ? (
-//             <Notification message="There is no feedback" />
-//           ) : (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={total}
-//               positivePercentage={percent}
-//             />
-//           )}
-//         </Section>
-//       </>
-//     );
-//   }
-// }
